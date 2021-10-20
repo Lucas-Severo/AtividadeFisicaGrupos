8f7fbebbbcc51e9f900bce7d01551628ebe5c2f3
gabriel.severo <gabriel.severo@azi.com.br>
2021-10-20 16:11:56 -0400

    Adicionado listagens das localidades e modalidades ao cadastro da atividade

--- src/main/java/com/edu/framework/atividadefisica/controller/AtividadeController.java
+++ src/main/java/com/edu/framework/atividadefisica/controller/AtividadeController.java
@@ -1,7 +1,13 @@
 package com.edu.framework.atividadefisica.controller;
 
+import java.util.List;
+
 import com.edu.framework.atividadefisica.dto.AtividadeRepository;
+import com.edu.framework.atividadefisica.dto.LocalidadeRepository;
+import com.edu.framework.atividadefisica.dto.ModalidadeRepository;
 import com.edu.framework.atividadefisica.model.Atividade;
+import com.edu.framework.atividadefisica.model.Localidade;
+import com.edu.framework.atividadefisica.model.Modalidade;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,12 +17,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequestMapping
 public class AtividadeController {
 
     @Autowired
     private AtividadeRepository atividadeRepository;
+
+    @Autowired
+    private LocalidadeRepository localidadeRepository;
+
+    @Autowired
+    private ModalidadeRepository modalidadeRepository;
 
     @GetMapping("/atividade")
     public String exibirListaModalidades() {
@@ -26,6 +39,12 @@ public class AtividadeController {
     @GetMapping("/cadastrarAtividade")
     public String exibirTelaCadastroAtividade(Model model) {
         model.addAttribute("atividade", new Atividade());
+
+        List<Localidade> localidades = localidadeRepository.findAll();
+        List<Modalidade> modalidades = modalidadeRepository.findAll();
+
+        model.addAttribute("localidades", localidades);
+        model.addAttribute("modalidades", modalidades);
         
         return "cadastrarAtividade";
     }
