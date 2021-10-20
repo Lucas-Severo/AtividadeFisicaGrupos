766556de13badcde3a1f1dc02c5688efe2a96bab
gabriel.severo <gabriel.severo@azi.com.br>
2021-10-20 17:01:34 -0400

    Adicionado validacao para localidade nao selecionada

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,4 +1,5 @@
-const localidadeInput = document.querySelector(".localidade")
+const localidadeInput = document.querySelector(".localidadeAutocomplete")
+const selectLocalidade = document.querySelector(".localidade")
 const localidades = {}
 
 function extrairLocalidades() {
@@ -8,6 +9,12 @@ function extrairLocalidades() {
     for(let localidade of localidadeOptions){
         localidades[localidade.textContent] = localidade.value
         localidade.removeAttribute("value")
+    }
+}
+
+function validarLocalidadeSelecionada(texto) {
+    if(!localidades[texto]){
+        localidadeInput.value = ""
     }
 }
 
@@ -23,6 +30,7 @@ function buscarElemento(elemento, texto) {
 }
 
 localidadeInput.addEventListener('blur', event => {
+    validarLocalidadeSelecionada(event.target.value)
     if(event.target.value){
         const id = localidades[event.target.value]
         const elemento = buscarElemento("option", event.target.value)
